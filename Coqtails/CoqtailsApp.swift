50b07fee6d3b7b523d8ef49699c2590ca0331a7d
Bruno Krolo <bruno.krolo@gmail.com>
2024-05-18 13:52:25 +0200

    Initial Commit

---
+++ Coqtails/CoqtailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoqtailsApp.swift
+//  Coqtails
+//
+//  Created by Bruno Krolo on 18.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct CoqtailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
